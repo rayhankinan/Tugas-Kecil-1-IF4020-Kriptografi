911b97bd3b44cf75dcab2501b2d7bd3e31082557
rayhankinan <rayhankinan@gmail.com>
2023-01-27 00:08:43 +0700

    Add dynamic func applicator

--- api/app/utils.py
+++ api/app/utils.py
@@ -6,4 +6,10 @@ async def apply_static_func_to_file(file: UploadFile, bytes_group: int = 1, func
     while chunk := await file.read(bytes_group):
         yield func(chunk)
 
-# TODO: Membuat iterable untuk mengaplikasikan dynamic func (digunakan pada auto-key dan enigma)
+
+async def apply_dynamic_func_to_file(file: UploadFile, bytes_group: int = 1, func: Callable[[bytes, int], bytes] = lambda x, _: x):
+    # TODO: Membuat iterable untuk mengaplikasikan dynamic func (digunakan pada auto-key dan enigma)
+    counter = 0
+    while chunk := await file.read(bytes_group):
+        yield func(chunk, counter)
+        counter += 1
