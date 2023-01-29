eee66507cd216c8cf23b0111c1b71a7c4ac149e7
rayhankinan <rayhankinan@gmail.com>
2023-01-29 23:20:35 +0700

    Add hill

--- api/app/hill/services.py
+++ api/app/hill/services.py
@@ -4,17 +4,73 @@ from typing import List
 from fastapi import UploadFile
 from .utils import HillKeyType
 from ..utils import AllByteType, apply_static_func_to_file
+from ..constants import LENGTH_OF_ALPHABET, OVERHEAD_ASCII
 
 
 async def encrypt_file_service(key: HillKeyType, file: UploadFile):
-    # TODO: Add Hill Encryption
+    # TODO: Add Hill Encryption (Not finished)
     async def encrypt_bytes(binary: AllByteType):
-        ...
-    return apply_static_func_to_file(file, )
+        key_matrix = np.array(key)
+        raw_matrix = key_matrix[0:len(binary)][0:len(binary)]
+
+        byte_list: List[bytes] = list(
+            unpack(f">{len(binary)}c", binary)
+        )
+        num_list = list(map(int, byte_list))
+        raw_vector = np.vstack(num_list)
+
+        remove_overhead = np.vectorize(lambda x: x - OVERHEAD_ASCII)
+        initial_matrix: np.ndarray = remove_overhead(raw_matrix)
+        initial_vector: np.ndarray = remove_overhead(raw_vector)
+
+        final_vector = initial_matrix @ initial_vector
+        final_array = final_vector.transpose()
+
+        modulo = np.vectorize(lambda x: x % LENGTH_OF_ALPHABET)
+        final_modulo_array: np.ndarray = modulo(final_array)
+
+        add_overhead = np.vectorize(lambda x: x + OVERHEAD_ASCII)
+        result_array: np.ndarray = add_overhead(final_modulo_array)
+        final_bytes = result_array.astype("uint8").tobytes()
+
+        return final_bytes
+
+    return apply_static_func_to_file(file, bytes_group=len(key), func=encrypt_bytes)
 
 
 async def decrypt_file_service(key: HillKeyType, file: UploadFile):
-    # TODO: Add Hill Decryption
+    # TODO: Add Hill Decryption (Not finished)
     async def decrypt_bytes(binary: AllByteType):
-        ...
-    return apply_static_func_to_file(file, )
+        key_matrix = np.array(key)
+        raw_matrix = key_matrix[0:len(binary)][0:len(binary)]
+
+        byte_list: List[bytes] = list(
+            unpack(f">{len(binary)}c", binary)
+        )
+        num_list = list(map(int, byte_list))
+        raw_vector = np.vstack(num_list)
+
+        remove_overhead = np.vectorize(lambda x: x - OVERHEAD_ASCII)
+        initial_matrix: np.ndarray = remove_overhead(raw_matrix)
+        initial_vector: np.ndarray = remove_overhead(raw_vector)
+
+        determinant: float = np.linalg.det(initial_matrix)
+        adjoint_matrix = determinant * np.linalg.inv(initial_matrix)
+        inverse_modulo = np.vectorize(
+            lambda x: pow(determinant, -1, LENGTH_OF_ALPHABET) * x
+        )
+        inverse_matrix: np.ndarray = inverse_modulo(adjoint_matrix)
+
+        final_vector = inverse_matrix @ initial_vector
+        final_array = final_vector.transpose()
+
+        modulo = np.vectorize(lambda x: x % LENGTH_OF_ALPHABET)
+        final_modulo_array: np.ndarray = modulo(final_array)
+
+        add_overhead = np.vectorize(lambda x: x + OVERHEAD_ASCII)
+        result_array: np.ndarray = add_overhead(final_modulo_array)
+        final_bytes = result_array.astype("uint8").tobytes()
+
+        return final_bytes
+
+    return apply_static_func_to_file(file, bytes_group=len(key), func=decrypt_bytes)
--- api/app/hill/utils.py
+++ api/app/hill/utils.py
@@ -1,6 +1,7 @@
 from typing import Type, List
 from pydantic import conlist
 from ..utils import PositiveIntegerType
+from ..constants import OVERHEAD_ASCII
 
 HillKeyType: Type[List[List[int]]] = conlist(
     conlist(
