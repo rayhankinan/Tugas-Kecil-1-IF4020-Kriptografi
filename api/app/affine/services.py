ae12124e17380943d8470e7e56aa6bf127075544
rayhankinan <rayhankinan@gmail.com>
2023-01-29 22:10:01 +0700

    Minor fix affine

--- api/app/affine/services.py
+++ api/app/affine/services.py
@@ -5,11 +5,11 @@ from ..constants import LENGTH_OF_ALPHABET, OVERHEAD_ASCII
 
 
 async def encrypt_file_service(key: PositiveIntegerType, shift: PositiveIntegerType, group: PositiveIntegerType, file: UploadFile):
-    # TODO: Add Affine Encryption (Bug ketika panjang bytes tidak divisible dengan group)
+    # TODO: Add Affine Encryption (Done)
     async def encrypt_bytes(binary: AlphabetByteType):
         raw_value = await binary_to_num(binary)
         initial_value = await repeat_subtract(raw_value, len(binary), OVERHEAD_ASCII)
-        modulo = await repeat_add(1, len(binary), LENGTH_OF_ALPHABET)
+        modulo = await repeat_add(0, len(binary), LENGTH_OF_ALPHABET)
         encrypted_value = (key * initial_value + shift) % modulo
         final_value = await repeat_add(encrypted_value, len(binary), OVERHEAD_ASCII)
         final_bytes = await num_to_binary(final_value, len(binary))
@@ -19,11 +19,11 @@ async def encrypt_file_service(key: PositiveIntegerType, shift: PositiveIntegerT
 
 
 async def decrypt_file_service(key: PositiveIntegerType, shift: PositiveIntegerType, group: PositiveIntegerType, file: UploadFile):
-    # TODO: Add Affine Decryption (Bug ketika panjang bytes tidak divisible dengan group)
+    # TODO: Add Affine Decryption (Done)
     async def decrypt_bytes(binary: AlphabetByteType):
         raw_value = await binary_to_num(binary)
         initial_value = await repeat_subtract(raw_value, len(binary), OVERHEAD_ASCII)
-        modulo = await repeat_add(1, len(binary), LENGTH_OF_ALPHABET)
+        modulo = await repeat_add(0, len(binary), LENGTH_OF_ALPHABET)
         decrypted_value = (pow(key, -1, modulo)
                            * (initial_value - shift)) % modulo
         final_value = await repeat_add(decrypted_value, len(binary), OVERHEAD_ASCII)
