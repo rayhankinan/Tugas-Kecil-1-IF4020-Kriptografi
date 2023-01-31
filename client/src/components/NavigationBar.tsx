330dc3cac0873e8a44d77355507d061a03975632
rayhankinan <rayhankinan@gmail.com>
2023-02-01 01:20:12 +0700

    Add data

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -1,19 +1,83 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import {
   AppBar,
   Box,
   Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
   Toolbar,
   Typography,
-  IconButton,
 } from "@mui/material";
-import { Menu as MenuIcon } from "@mui/icons-material";
+import {
+  Menu as MenuIcon,
+  EnhancedEncryption as EnhancedEncryptionIcon,
+} from "@mui/icons-material";
+
+interface PageLink {
+  name: string;
+  url: string;
+}
 
 const NavigationBar: React.FC = () => {
   const [drawerOpened, setDrawerOpened] = React.useState(false);
 
   const toggleDrawer = () => {
     setDrawerOpened(!drawerOpened);
+  };
+
+  const navigate = useNavigate();
+
+  const pageLinks: PageLink[] = [
+    {
+      name: "Affine",
+      url: "/affine",
+    },
+    {
+      name: "Auto Key Vigenere",
+      url: "/auto-key-vigenere",
+    },
+    {
+      name: "Enigma",
+      url: "/enigma",
+    },
+    {
+      name: "Extended Vigenere",
+      url: "/extended-vigenere",
+    },
+    {
+      name: "Hill",
+      url: "/hill",
+    },
+    {
+      name: "Playfair",
+      url: "/playfair",
+    },
+    {
+      name: "Vigenere",
+      url: "/vigenere",
+    },
+  ];
+
+  const generateList = (pageLinks: PageLink[]) => {
+    return (
+      <List>
+        {pageLinks.map((pageLink) => (
+          <ListItem key={pageLink.name} disablePadding>
+            <ListItemButton onClick={() => navigate(pageLink.url)}>
+              <ListItemIcon>
+                <EnhancedEncryptionIcon />
+              </ListItemIcon>
+              <ListItemText primary={pageLink.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    );
   };
 
   return (
@@ -34,7 +98,7 @@ const NavigationBar: React.FC = () => {
             Tugas Kecil 1 IF4020 Kriptografi
           </Typography>
           <Drawer anchor="left" open={drawerOpened} onClose={toggleDrawer}>
-            {/* TODO: Navigation List */}
+            {generateList(pageLinks)}
           </Drawer>
         </Toolbar>
       </AppBar>
