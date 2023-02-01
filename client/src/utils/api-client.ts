d0f16311590812fcc581b10d9ef00338b8615936
rayhankinan <rayhankinan@gmail.com>
2023-02-01 15:36:03 +0700

    Add file stream

--- client/src/utils/api-client.ts
+++ client/src/utils/api-client.ts
@@ -1,8 +1,6 @@
 import Axios, { AxiosError } from "axios";
-
-interface APIResponse {
-  file: Blob[];
-}
+import stream from "stream";
+import fs from "fs";
 
 interface APIErrorDetail {
   loc: string[];
@@ -14,32 +12,57 @@ interface APIError {
   detail: APIErrorDetail[];
 }
 
+class FileStream extends stream.Writable {
+  private readonly fileBits: BlobPart[];
+  private readonly fileName: string;
+
+  constructor(fileName: string) {
+    super();
+    this.fileBits = [];
+    this.fileName = fileName;
+  }
+
+  _write(
+    chunk: any,
+    encoding: BufferEncoding,
+    callback: (error?: Error | null | undefined) => void
+  ): void {
+    this.fileBits.push(chunk as BlobPart);
+    callback();
+  }
+
+  getFile() {
+    return new File(this.fileBits, this.fileName);
+  }
+}
+
 class APIClient {
   public static readonly API_HOST = process.env.API_HOST || "localhost";
   public static readonly API_KEY = process.env.API_KEY || "api-key";
 
   private static HandleError(rawError: any): APIError {
-    const error: APIError = {
+    const emptyError: APIError = {
       detail: [],
     };
 
     if (rawError instanceof AxiosError) {
       const { response } = rawError as AxiosError;
       if (response) {
-        error.detail = (response.data as APIError).detail;
+        return response.data as APIError;
       }
+      return emptyError;
     }
-
-    return error;
+    return emptyError;
   }
 
   public static async Post(
     path: string,
     query: Record<string, string>,
-    body: Record<string, File>
-  ): Promise<APIResponse | APIError> {
+    body: Record<string, File>,
+    fileName: string
+  ): Promise<File | APIError> {
     try {
-      const response = await Axios.post<APIResponse>(
+      const response = await Axios.post<fs.ReadStream>(
         `${APIClient.API_HOST}${path}`,
         body,
         {
@@ -47,10 +70,14 @@ class APIClient {
             access_token: APIClient.API_KEY,
             ...query,
           },
+          responseType: "stream",
         }
       );
 
-      return response.data;
+      const fileStream = new FileStream(fileName);
+      response.data.pipe(fileStream);
+
+      return fileStream.getFile();
     } catch (err: any) {
       return APIClient.HandleError(err);
     }
